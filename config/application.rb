b31d11820cbb44f3a53e9838081fae9f3ea233eb
mwai <mwmtingwi5@gmail.com>
2015-09-10 11:29:25 +0200

    modified:   app/views/clients/show.html.erb 	modified:   app/views/clients/show.html.erb~ 	modified:   config/application.rb 	modified:   config/environment.rb 	modified:   log/development.log 	modified:   log/newrelic_agent.log 	modified:   log/passenger.3000.log 	deleted:    tmp/pids/passenger.3000.pid

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require "fileutils"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
-module Sellies
+module edi
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-Sellies::Application.initialize!
+Edi::Application.initialize!
 
 
