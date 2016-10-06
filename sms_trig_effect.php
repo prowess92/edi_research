e26c85032c8568bbe704c876f6235ed1a6336578
Edith Kumwenda <egulule@gmail.com>
2016-10-06 08:13:52 +0200

    modified:   data_appointments.php 	new file:   data_appointments.php~ 	modified:   log/development.log 	modified:   sms_api.php 	new file:   sms_api.php~ 	modified:   sms_trig.php 	modified:   sms_trig.php~

--- sms_trig_effect.php
+++ sms_trig_effect.php
@@ -3,7 +3,7 @@
 include 'sms_api.php'; 
 
 
-	$number = array('0993030442', '0999814793', '0999952622', '0888149994', '0999771333', '0999933910', '0993030434', '0996234931', '0999758400');        
+	$number = array('0888722434');        
 	
 	$count = count($number);
 	 
@@ -15,7 +15,7 @@ include 'sms_api.php';
 	//for daily sms reminders change value of $message_effect1 and $message_effect2 variable
 	$message_effect1 = "Mukumbukire kuti zizindikiro zosonyeza kuti mwadana ndi makhwala, ndi chikasu m'maso, zilonda pa khungu ndi dzanzi m'manja ndi mmiyendo";
 
-	$message_effect2 = "Mukumbukire kuti zizindikiro za makhwala zosaopsya ndi chizungulire, maloto achilendo ndi nselu";
+	$message_effect2 = "Mukumbukire kuti zizindikiro za makhwala zosaopsya ndi:- Chizungulire, Maloto a chilendo ndi Nselu, ndipo zimenezi zimasiya zokha";
        
 	$sms_api_result = sendSMS($current_number, $message_effect1);
 	print_r($sms_api_result);
